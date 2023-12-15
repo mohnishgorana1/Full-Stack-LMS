d76fa625a5e55c4c0fa6a2b91b9c23b47bba6184
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-15 16:45:21 +0530

    course model schema defind with some validations

--- server/src/models/course.model.js
+++ server/src/models/course.model.js
@@ -3,19 +3,30 @@ const courseSchema = new Schema(
   {
     title: {
       type: String,
+      required: [true, "title is required"],
+      minLength: [8, "title must be at least 8 characters"],
+      maxLength: [59, "title must be less than 60 characters"],
+      trim: true,
     },
     description: {
       type: String,
+      required: [true, "description is required"],
+      minLength: [10, "description must be at least 10 characters"],
+      maxLength: [200, "description must be less than 201 characters"],
+      trim: true,
     },
     category: {
       type: String,
+      required: [true, "Category is required"],
     },
     thumbnail: {
       public_id: {
         type: String,
+        required: true,
       },
       secure_url: {
         type: String,
+        required: true,
       },
     },
     lectures: [
@@ -25,18 +36,23 @@ const courseSchema = new Schema(
         lecture: {
           public_id: {
             type: String,
+            required: true,
           },
           secure_url: {
             type: String,
+            required: true,
           },
         },
       },
     ],
     numberOfLectures: {
-        type: Number,
+      type: Number,
+      default: 0,
     },
     createdBy: {
-        type: String,
+      type: String,
+      required: true,
+
     },
   },
   { timestamps: true }
