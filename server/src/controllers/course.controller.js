0501f928b23f4f3ce40bb9c1032bfb278cf9d0f0
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-16 20:39:21 +0530

    some routes defined for course and made controller

--- server/src/controllers/course.controller.js
+++ server/src/controllers/course.controller.js
@@ -1,5 +1,7 @@
 import { Course } from "../models/course.model.js";
 import AppError from "../utils/error.util.js";
+import cloudinary, { v2 } from "cloudinary";
+import fs from "fs/promises";
 
 const getAllCourses = async (req, res, next) => {
   try {
@@ -34,4 +36,49 @@ const getLecturesByCourseId = async (req, res, next) => {
   }
 };
 
-export { getAllCourses, getLecturesByCourseId };
+const createCourse = async (req, res, next) => {
+  const { title, description, category, createdBy } = req.body;
+
+  if (!title || !description || !category || !createdBy) {
+    return next(new AppError("All fields are requires", 400));
+  }
+
+  const course = await Course.create({
+    title,
+    description,
+    category,
+    createdBy,
+  });
+  if (!course) {
+    return next(new AppError("Error creating the course", 500));
+  }
+
+  if (req.file) {
+    const result = await cloudinary.v2.uploader.upload(req.file.path, {
+      folder: "lms",
+    });
+    if (result) {
+      course.thumbnail.public_id = result.public_id;
+      course.thumbnail.secure_url = result.secure_url;
+    }
+    fs.rm(`uploads/${req.file.filename}`);
+  }
+
+  await course.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Course created Successfully",
+  });
+};
+
+const updateCourse = async (req, res, next) => {};
+const deleteCourse = async (req, res, next) => {};
+
+export {
+  getAllCourses,
+  getLecturesByCourseId,
+  createCourse,
+  updateCourse,
+  deleteCourse,
+};
--- server/src/routes/course.routes.js
+++ server/src/routes/course.routes.js
@@ -1,15 +1,25 @@
 import express from "express";
 import {
+  createCourse,
+  deleteCourse,
   getAllCourses,
   getLecturesByCourseId,
+  updateCourse,
 } from "../controllers/course.controller.js";
 import { isLoggedIn } from "../middlewares/jwtAuth.middleware.js";
 
-
 const router = express.Router();
 
-router.route("/").get(getAllCourses);
+router
+  .route("/")
+  .get(getAllCourses)
+  .post(upload.single("thumbnail"), createCourse);
 
-router.route("/:id").get(isLoggedIn, getLecturesByCourseId);
+  
+router
+  .route("/:id")
+  .get(isLoggedIn, getLecturesByCourseId)
+  .put(updateCourse)
+  .delete(deleteCourse);
 
 export default router;
