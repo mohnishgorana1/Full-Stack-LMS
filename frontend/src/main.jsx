ecb00c04bb0aa94b1564df7f36faea0d2d5a662c
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-17 23:59:42 +0530

    toaster added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {Toaster} from 'react-hot-toast'
 import {BrowserRouter} from 'react-router-dom'
 
 import App from './App.jsx'
@@ -10,6 +11,7 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <App />
+        <Toaster />
     </BrowserRouter>
 
 )
