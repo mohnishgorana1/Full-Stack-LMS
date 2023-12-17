a33c5e6a5bb78acf74356357b7df6992bba9227d
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-17 23:57:39 +0530

    eslint configs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
+import { Routes } from "react-router-dom";
+
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  <>
+    <Routes>
+
+      
+    </Routes>
+  </>
+  
 }
 
 export default App;
