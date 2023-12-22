22527cd34fd9e31de66cb4d9d7c4178c9c746877
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-22 22:27:09 +0530

    register functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,16 @@ import Footer from "./Components/Footer";
 import AboutUs from "./Pages/AboutUs";
 import HomePage from "./Pages/HomePage";
 import NotFound from "./Pages/NotFound";
+import Signup from "./Pages/Signup";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
 
         <Route path="*" element={<NotFound />}></Route>
-
-
       </Routes>
       <Footer />
     </>
