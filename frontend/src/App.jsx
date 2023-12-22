14752a81346913e3c6242898cec0a762db86fd3e
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-22 13:24:06 +0530

    not found page made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import AboutUs from "./Pages/AboutUs";
 import HomePage from "./Pages/HomePage";
+import NotFound from "./Pages/NotFound";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
+
+        <Route path="*" element={<NotFound />}></Route>
+
 
       </Routes>
       <Footer />
